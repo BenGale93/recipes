8709fb265e80e1df545990787f79aa2c3d9aa76c
Ben Gale <bengale2007@googlemail.com>
2023-05-06 16:28:50 +0100

    Made use of minijinja

--- src/main.rs
+++ src/main.rs
@@ -1,46 +1,75 @@
 #![warn(clippy::all, clippy::nursery)]
-use axum::extract::State;
-use axum::{response::Html, routing::get, Router};
-use serde::Deserialize;
-use serde::Serialize;
-use std::net::SocketAddr;
-use std::sync::Arc;
+#![allow(dead_code)]
+use std::{net::SocketAddr, sync::Arc};
 
-#[derive(Debug, Deserialize, Serialize)]
-struct Recipe {
+use axum::{extract::State, response::Html, routing::get, Router};
+use minijinja::{context, Environment};
+use serde::{Deserialize, Serialize};
+
+const HOME_TEMPLATE: &str = r#"
+<body style='padding: 30px;'>
+<div>
+    <ul>
+    {% for recipe in recipes %}
+        <li><a href='#{{recipe.anchor}}'>{{recipe.name}}</a></li>
+    {% endfor %}
+    </ul>
+</div>
+{% for recipe in recipes %}
+    <div id='{{recipe.anchor}}'>
+        <h1>{{recipe.name}}</h1>
+        <ul>
+            {% for item in recipe.ingredients %}
+            <li>{{ item }}</li>
+            {% endfor %}
+        </ul>
+        <p style='white-space: pre-line;'>
+            {{recipe.recipe}}
+        </p>
+    </div>
+{% endfor %}
+</body>
+"#;
+
+#[derive(Deserialize)]
+struct DeserializeRecipe {
     name: String,
     ingredients: Vec<String>,
     recipe: String,
 }
 
+impl From<DeserializeRecipe> for Recipe {
+    fn from(tmp: DeserializeRecipe) -> Self {
+        Self::new(tmp.name, tmp.ingredients, tmp.recipe)
+    }
+}
+
+#[derive(Debug, Serialize, Deserialize, Clone)]
+#[serde(from = "DeserializeRecipe")]
+struct Recipe {
+    name: String,
+    ingredients: Vec<String>,
+    recipe: String,
+    #[serde(skip_serializing)]
+    anchor: String,
+}
+
 impl Recipe {
-    fn html_link(&self) -> String {
-        let href = format!("#{}", self.name.replace(' ', "-"));
-        format!("<li><a href='{}'>{}</a></li>", href, self.name)
-    }
-
-    fn to_html(&self) -> String {
-        let anchor = self.name.replace(' ', "-");
-        format!(
-            "<div id='{}'><h1>{}</h1><p>{}</p><p>{}</p></div>",
+    fn new(name: String, ingredients: Vec<String>, recipe: String) -> Self {
+        let anchor = name.replace(' ', "-");
+        Self {
+            name,
+            ingredients,
+            recipe,
             anchor,
-            self.name,
-            self.ingredients.join("<br>"),
-            self.recipe.replace('\n', "<br>")
-        )
+        }
     }
 }
 
-fn create_links(recipes: &[Recipe]) -> String {
-    let links: Vec<String> = recipes.iter().map(|r| r.html_link()).collect();
-    let links = links.join("");
-    format!("<div><ul>{}</ul></div>", links)
-}
-
-fn create_bodies(recipes: &[Recipe]) -> String {
-    let bodies: Vec<String> = recipes.iter().map(|r| r.to_html()).collect();
-    let bodies = bodies.join("");
-    format!("<div>{}</div>", bodies)
+#[derive(Clone)]
+struct AppState<'a> {
+    pub recipes: Arc<Vec<Recipe>>,
+    pub env: Arc<Environment<'a>>,
 }
 
 #[tokio::main]
@@ -48,15 +77,21 @@ async fn main() {
     let recipes_string = std::fs::read_to_string("recipes.yaml").expect("Expected file to exist.");
     let recipes_yaml = serde_yaml::from_str(&recipes_string).expect("Expected to parse YAML.");
     let recipes: Arc<Vec<Recipe>> = Arc::new(recipes_yaml);
+
+    let mut env = Environment::new();
+    env.set_debug(true);
+    env.add_template("home", HOME_TEMPLATE).unwrap();
+
+    let app_state = Arc::new(AppState {
+        recipes,
+        env: Arc::new(env),
+    });
+
     let app = Router::new()
         .route("/", get(display_recipes))
-        .with_state(recipes);
+        .with_state(app_state);
 
-    // run it
-    #[cfg(not(debug_assertions))]
-    let addr = SocketAddr::from(([192, 168, 1, 92], 3000));
-    #[cfg(debug_assertions)]
-    let addr = SocketAddr::from(([127, 0, 0, 1], 3000));
+    let addr = SocketAddr::from(([0, 0, 0, 0], 3000));
 
     println!("listening on {}", addr);
     axum::Server::bind(&addr)
@@ -65,12 +100,13 @@ async fn main() {
         .unwrap();
 }
 
-async fn display_recipes(State(recipes): State<Arc<Vec<Recipe>>>) -> Html<String> {
-    let links = create_links(&recipes);
-    let bodies = create_bodies(&recipes);
+#[allow(clippy::unnecessary_to_owned)]
+async fn display_recipes(State(app_state): State<Arc<AppState<'_>>>) -> Html<String> {
+    let template = app_state.env.get_template("home").unwrap();
 
-    Html(format!(
-        "<body style='padding: 30px;'>{}{}</body>",
-        links, bodies
-    ))
+    let r = template
+        .render(context!(recipes => app_state.recipes.to_vec()))
+        .unwrap();
+
+    Html(r)
 }
